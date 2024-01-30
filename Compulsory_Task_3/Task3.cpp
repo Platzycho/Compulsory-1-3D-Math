bde435c3950d552108c63e30cadc9dddaca6725f
Platzycho <Neslin_Lime@outlook.com>
2024-01-30 14:10:37 +0100

    Task_3_Redo

---
+++ Compulsory_Task_3/Task3.cpp
@@ -0,0 +1,30 @@
+#include "Task3.h"
+#include <fstream>
+
+double Task_3::f(double x, double y)
+{
+	return 5 * pow(x ,3) + 2 * y;
+}
+
+std::vector<std::vector<double>> Task_3::computeSurface(int resolution)
+{
+	std::ofstream outFile("output.txt");
+	if(!outFile.is_open()) {
+		std::cerr << "Unable to open file for writing." << std::endl;
+	}
+    
+	std::vector<std::vector<double>> surface(resolution, std::vector<double>(resolution));
+	double x, y;
+	double step = 10.0f / (resolution - 1);
+
+	for(int i = 0; i < resolution; ++i){
+		x = i * step;
+		for(int j = 0; j < resolution; ++j){
+			y = j * step;
+			surface[i][j] = f(x, y);
+			outFile << "Random words: " << f(x, y) << std::endl;
+		}
+	}
+	return surface;
+	
+}
